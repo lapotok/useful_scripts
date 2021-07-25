1f5b1496902f52eee620d979543ce88cd819dc6b
lapotok <lapotok@gmail.com>
2021-07-25 16:20:50 +0300

    find_duplicates.py: added two methods and caching

--- misc/find_duplicates.py
+++ misc/find_duplicates.py
@@ -1,14 +1,20 @@
-import sys
-import pathlib, glob
+#!/usr/bin/python3
 
+import sys, os
+import pathlib, glob
+from joblib import Memory
+
+memory = Memory('_joblib_cache_', verbose=0)
+
+@memory.cache
 def md5sum(filename):
     import subprocess
-    result = subprocess.run(f'md5 {filename}', shell=True, capture_output=True)
+    result = subprocess.run(f"md5 '{filename}'", shell=True, capture_output=True)
     return result.stdout.decode().strip().split(' ')[-1]
 
-def find_duplicates(dir_list):
+def find_duplicates(dir_list, method='md5'): # or filename
     
-    md5_fn_dict = {}
+    key_dict = {}
     
     for dir_i in dir_list:
 
@@ -18,16 +24,17 @@ def find_duplicates(dir_list):
         current_n = 1
         sys.stderr.write(f'# Dir: {dir_i}\n')
         for f in files:
-            md5_f = md5sum(f)
-            if not md5_f in md5_fn_dict:
-                md5_fn_dict[md5_f] = []
-            md5_fn_dict[md5_f].append(f)
-            if current_n % 500 == 0:
-                sys.stderr.write(f'Processing #{current_n} of {total_n}\n')
-            current_n += 1
+            if os.path.isfile(f):
+                key = md5sum(f) if method == 'md5' else os.path.basename(f)
+                if not key in key_dict:
+                    key_dict[key] = []
+                key_dict[key].append(f)
+                if current_n % 500 == 0:
+                    sys.stderr.write(f'Processing #{current_n} of {total_n}\n')
+                current_n += 1
         sys.stderr.write('Done.\n\n')
 
-    duplicates = [v for v in md5_fn_dict.values() if len(v)>1]
+    duplicates = [v for v in key_dict.values() if len(v)>1]
     
     if len(duplicates)>0:
         print('Duplicates found:')
@@ -39,11 +46,18 @@ def find_duplicates(dir_list):
             print('\n'.join(d))
 
 if __name__ == '__main__':
+    
 
-    if len(sys.argv)<2:
-        print('Not enough args!')
+    if len(sys.argv)<3:
+        print(f'Not enough args!\n\nUsage: {sys.argv[0]} [-f|-m] dirs')
         sys.exit(2)
 
-    dir_list = sys.argv[1:]
+    dir_list = sys.argv[2:]
 
-    find_duplicates(dir_list)
+    if sys.argv[1] == '-m':
+        find_duplicates(dir_list, 'md5')
+    elif sys.argv[1] == '-f':
+        find_duplicates(dir_list, 'filename')
+    else:
+        print('What method?')
+        sys.exit(2)
